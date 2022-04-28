cacdb3575ee09e0eb32296e012fbd941a2458ae4
KsuChe <nerro@mail.ru>
2022-04-28 21:17:44 +0300

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println();
+//task 1
+        int[] weights = new int[3];
+
+        float[] weight = {1.57f, 7.654f, 9.986f};
+
+        boolean[] bools = new boolean[3];
     }
 }
