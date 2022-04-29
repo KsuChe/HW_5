3a187aa47fdd0725f7f7fa26cd749a86830bd01d
KsuChe <nerro@mail.ru>
2022-04-29 20:23:59 +0300

    3

--- src/Main.java
+++ src/Main.java
@@ -4,28 +4,55 @@ public class Main {
         System.out.println();
 //task 1,2
         int[] weights = new int[3];
-        weights [0] = 1;
-        weights [1] = 2;
-        weights [2] = 3;
-        for (int i = 0; i<3; i++){
-            System.out.print(weights[i] + ",");
-        }
+        weights[0] = 1;
+        weights[1] = 2;
+        weights[2] = 3;
+        System.out.print(weights[0] + "," + weights[1] + "," + weights[2]);
+
         System.out.println();
         float[] weight = {1.57f, 7.654f, 9.986f};
-        weight [0] = 1.57f;
-        weight [1] = 7.654f;
-        weight [2] = 9.986f;
-        for (int n = 0; n< weight.length; n++){
-            System.out.print(weight[n] + ",");
-        }
+        weight[0] = 1.57f;
+        weight[1] = 7.654f;
+        weight[2] = 9.986f;
+        System.out.print(weight[0] + "," + weight[1] + "," + weight[2]);
+
         System.out.println();
-        long [] number = new long[2];
-        number [0] = 1;
-        number [1] = 2;
-        for (int l = 0; l<2; l++) {
-            System.out.print(number[l] + ",");
+        long[] number = new long[2];
+        number[0] = 1;
+        number[1] = 2;
+
+        System.out.print(number[0] + "," + number[1]);
+
+        System.out.println();
+        //task 3
+
+        weights[0] = 3;
+        weights[1] = 2;
+        weights[2] = 1;
+        System.out.print(weights[0] + "," + weights[1] + "," + weights[2]);
+
+        System.out.println();
+
+        weight[0] = 9.986f;
+        weight[1] = 7.654f;
+        weight[2] = 1.57f;
+        System.out.print(weight[0] + "," + weight[1] + "," + weight[2]);
+        System.out.println();
+
+        number[0] = 2;
+        number[1] = 1;
+        System.out.print(number[0] + "," + number[1]);
+        System.out.println();
+
+        //task4
+
+        for (int i = 0; i < weights.length; i++) {
+            if (weights [i] % 2 == 0) {
+                System.out.println(weights[i] + ",");
+            }else if (weights [i] % 2 != 0) {
+                System.out.print(++weights[i] +",");
+            }
+
         }
-
-
     }
 }
